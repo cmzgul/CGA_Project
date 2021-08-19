466f691175317a541ae94e4759972b7321176ee1
cmzgul <fatmaguel.coemez@smail.th-koeln.de>
2021-08-19 11:44:39 +0200

    Normal Shader generiert

--- project/CGAFramework/assets/shaders/normal_vert.glsl
+++ project/CGAFramework/assets/shaders/normal_vert.glsl
@@ -1,7 +1,6 @@
 #version 330 core
 
 layout(location = 0) in vec3 position;
-layout(location = 1) in vec2 texCoords;
 layout(location = 2) in vec3 normal;
 
 //uniforms
@@ -10,9 +9,7 @@ uniform mat4 model_matrix;
 uniform mat4 view;
 uniform mat4 perspective;
 
-uniform vec2 tcMultiplier;
 
-uniform vec3 PointLightPosition, SpotLightFrontPosition;
 
 out vec3 Normal;
 
