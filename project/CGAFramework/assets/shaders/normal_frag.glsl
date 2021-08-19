b14268be5e358be2429f22c68976b37e303857d5
cmzgul <fatmaguel.coemez@smail.th-koeln.de>
2021-08-19 11:45:54 +0200

    Fragment Normal Shader generiert

--- project/CGAFramework/assets/shaders/normal_frag.glsl
+++ project/CGAFramework/assets/shaders/normal_frag.glsl
@@ -4,18 +4,7 @@
 
 in vec3 Normal;
 
-struct Material{
-    sampler2D diff;
-    sampler2D emit;
-    sampler2D specular;
-    float shininess;
-};
 
-
-uniform Material material;
-uniform vec3 PointLightColor, PointLightAttenuationFactors;
-uniform vec3 SpotLightFrontColor, SpotLightFrontDirection, SpotLightFrontAttenuationFactors;
-uniform float SpotLightFrontOuterAngle, SpotLightFrontInnerAngle;
 
 //fragment shader output
 out vec4 color;
